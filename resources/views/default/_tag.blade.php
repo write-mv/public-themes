1350bfa37c4a8149dfc208ff4e99a70475127f9b
Mohamed jinas <j@live.mv>
2021-09-20 08:33:39 +0500

    remove desc

--- resources/views/default/_tag.blade.php
+++ resources/views/default/_tag.blade.php
@@ -4,7 +4,6 @@
         <div class="mt-10 mb-12 text-center">
             <h2 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900 hover:underline"
                 style="font-family: Poppins;"> {{ $tag->name }}</h2>
-            <p class="text-lg text-gray-500" style="font-family: Poppins;">{{ $blog->description }}</p>
         </div>
         <div class="p-6 mt-6">
             <div class="flex flex-col lg:grid lg:gap-4 2xl:gap-6 2xl:pb-8 ml-2 pt-4 px-6">
